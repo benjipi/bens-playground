f1ac117633c98209ee49ced250afcf8678ac294f
benrm <>
2021-11-01 04:27:52 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+"""
+This is the Template Repl for Python with Turtle.
+
+Python with Turtle lets you make graphics easily in Python.
+
+Check out the official docs here: https://docs.python.org/3/library/turtle.html
+"""
+
+import turtle
+
+t = turtle.Turtle()
+
+
+for col in ['red', 'green', 'yellow', 'blue']:
+  t.color(col)
+  t.forward(50) 
+  t.left(90)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
